ce881a61be351d21267e303da3085e7cdbfa23e6
Jackwhitener <jackwhitener1@gmail.com>
2018-10-16 13:11:50 -0400

    oops, forgot to push for four hours, gosh darnit.

--- Roman_test.rb
+++ Roman_test.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require_relative "roman.rb"
+require_relative "arabic.rb"
 class My_test < Minitest::Test
 	def test_one
 		assert_equal("I", 1.to_roman)
@@ -12,5 +13,17 @@ class My_test < Minitest::Test
 	end
 	def test_four
 		assert_equal("MCMLXXXIX", 1989.to_roman)
+	end
+	def test_five
+		assert_equal(3, "III".to_arabic)
+	end
+	def test_six
+		assert_equal(9, "IX".to_arabic)
+	end
+	def test_seven
+		assert_equal(10, "X".to_arabic)
+	end
+	def test_eight
+		assert_equal(1989, "MCMLXXXIX".to_arabic)
 	end
 end
